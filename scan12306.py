4483723fc111c34e90a21e9142681591dc7d60d9
branchelee <lxyx520123@163.com>
2016-07-31 22:08:26 +0800

    完成V1.0，实现输入出发城市、到达城市、时间、输出车次信息

--- scan12306.py
+++ scan12306.py
@@ -1,47 +1,17 @@
-#coding:utf-8
+# -*- coding: UTF-8 -*-
 import re,urllib,urllib2,requests
 import ssl,simplejson
 #simplejson用于将str格式的{}转为dict格式
 from functools import wraps
 from PIL import Image
+import position12306
+
 #获取12306剩余车票数量
 
 url = 'https://kyfw.12306.cn/otn/lcxxcx/init'
 
-def Savetofile(endpage):#保存文件到txt，参数为结束页页号
-	global urllist1
-	for page in range (1,endpage+1):
-		print 'getting page %d' %page
-		url = 'https://www.xztrt.com/invest/index.html?page=%d' %page
-		urllist1 = urllist1 + list(set(GetProductURL(url)))
-	print len(urllist1)
-	f = open("trtproduct.txt",'wb')
-	for i in urllist1:
-		i = 'https://www.xztrt.com/'+i
-		f.write(i+'\n')
-	f.close
-# Savetofile(2)
-def logintrt():
-	ProductURL = 'https://www.xztrt.com//invest/detail.html?id=b4716901-9d71-47ef-a3c6-40b0047bc367'
-	# print requests.get(url11).text
-	validpage = 'https://www.xztrt.com/validimg.html'
-	loginurl = 'https://www.xztrt.com/user/login.html'
 
-	s = requests.session()
-	#获取验证码并保存到本地
-	urllib.urlretrieve(validpage,r"valid"+".jpg")
-	Image.open(r"valid.jpg").show()
-	#输入验证码
-	validCode = raw_input('please input validcode')
-	# validCode = "1234"
-	data = {'user':'test111','password':'lxyx520123','validCode':validCode}
-	
-	#post 换成登录的地址，
-	res=s.post(loginurl,data)
-	#换成抓取的地址
-	
-	req = s.get(ProductURL)
-	print req.text
+
 def get12306():
 	validpage = 'https://www.xztrt.com/validimg.html'
 	url = 'https://kyfw.12306.cn/otn/lcxxcx/init'
@@ -89,20 +59,65 @@ def get_train_list(querydate,from_station,to_station):
 	re1 = r'{.+?}'
 	recompile = re.compile(re1)
 	#找到网页内所有的车次，从85位字符起读
-	listall = re.findall(recompile, req.content[85:-1])
+	# htmluni = req.content[85:-1]
+	# htmlutf = htmluni.encode('utf-8')
+	html = req.content[85:-1]
+	listall = re.findall(recompile,html)
 	# print listall[0]
 	#把listall里面的转为dict格式方便读写
 	for i in range(0,len(listall)):
 		listall[i] = simplejson.loads(listall[i])
 
 	return listall
-querydate = '2016-07-29'
-from_station = 'CUW'
-to_station = 'ICW'
-listall = get_train_list(querydate,from_station,to_station)
+#从list的所有dict中输出序号车次等信息
+def output(listall):
+	for i in range(0,len(listall)):
+		print '序号%d,车次：%s，出发时间：%s，到达时间%s，一等座%s，二等座%s'\
+				%	(i,\
+					listall[i]['station_train_code'],\
+					listall[i]['start_time'],\
+					listall[i]['arrive_time'],\
+					listall[i]['zy_num'].encode('utf-8'),\
+					listall[i]['ze_num'].encode('utf-8'),\
+					)
 
-print listall[4]['station_train_code']
+querydate1 = '2016-08-16'
+from_station_chn = raw_input('from where:')
+print from_station_chn
 
+from_station = position12306.find_position_names(from_station_chn)
+print from_station
+to_station_chn = raw_input('去哪儿:')
+querydate = raw_input('哪天出发，例如2016-08-12:')
+
+from_station = position12306.find_position_names(from_station_chn)
+print 'from_station is:',from_station
+to_station = position12306.find_position_names(to_station_chn)	
+print 'to_station is:',to_station
+	# print position_eng
+listall = get_train_list(querydate1,from_station,to_station)
+
+output(listall)
+'''
+station_train_code = '车次'
+start_time = '出发时间'
+arrive_time = '到达时间'
+from_station_name = '出发站'
+to_station_name = '到达站'
+swz_num = '商务座'
+tz_num = '特等座'
+zy_num = '一等座'
+ze_num = '二等座'
+gr_num = '高级软卧'
+rw_num = '软卧'
+yw_num = '硬卧'
+rz_num = '软座'
+yz_num = '硬座'
+wz_num = '无座'
+
+
+
+'''
 # validpage = 'https://www.xztrt.com/validimg.html'
 # urllib.urlretrieve(validpage,r"valid"+".jpg")
 # validpage = requests.get(validpage)
